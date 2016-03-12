e1c56f9fd1cbb87d822c4f9d9676d25137890745
Geocali <clement.vial@geocali.com>
2016-03-12 13:51:33 -0300

    adaptation for linux

--- georef.py
+++ georef.py
@@ -6,19 +6,11 @@ import csv
 def georef(dossier_in, dossier_out, srs1, srs2):
 	# ========= on convertit les points
 	
-	file_input=dossier_in.replace('\\','/') + '/pts_carres.csv'
-	file_output=dossier_out.replace('\\','/') + '/pts_georef.csv'
+	file_input=dossier_in + '/pts_carres.csv'
+	file_output=dossier_out + '/pts_georef.csv'
 	
-	#name='C:\\georef\\test6\\command.bat'
-	name=dossier_out + '\\command.bat'
 	command='gdaltransform "-s_srs" ' + srs1 + ' -t_srs ' + srs2 + ' < ' + file_input + ' > ' + file_output
-	file = open(name,'w')   # Trying to create a new file or open one
-	file.write('@ECHO OFF\n')
-	file.write('SET Path="c:\program files\qgis pisa\\bin\";%PATH%\n') # !!!!!!!!!!!!!!!!!!!!!!!
-	file.write('SET GDAL_DATA="c:\georef"\n') # !!!!!!!!!!!!!!!
-	file.write(command)
-	file.close()
-	os.system(name)
+	os.system(command)
 	
 	csvfile=open(file_output, 'r')
 	reader=csv.reader(csvfile, delimiter=" ")
@@ -39,7 +31,7 @@ def georef(dossier_in, dossier_out, srs1, srs2):
 			nom=nom_image[:-4]
 			decomp=nom.split('-')
 			nb=len(decomp)
-			images.append(['"' + dossier_in.replace('\\','/') + '/' + nom_image + '"', int(decomp[nb-2]), int(decomp[nb-1])])
+			images.append([dossier_in + '/' + nom_image, int(decomp[nb-2]), int(decomp[nb-1])])
 	# --- liste des points de géoréférencement
 	srs1='"+proj=latlong +ellps=WGS84 +datum=WGS84 +no_defs"'
 	ullon2=[]
@@ -55,26 +47,21 @@ def georef(dossier_in, dossier_out, srs1, srs2):
 		lrlon=pts_ref[k+1][0]
 		lrlat=pts_ref[k+1][1]
 		
-		file1=im[0].replace('/','\\')
-		file2=im[0][:-5] + '.tif'
-		file2=file2[1:]
+		file1=im[0]
+		file2=file1.replace('2_printed_images', '3_georef_images')
+		file2=file2.replace('png', 'tif')
 		n=n+1
-		print('georeferencement image ' + str(n) + '/' + str(len(images))) # !!!!!!!!!!!!!!
+		print('georeferencement image ' + str(n) + '/' + str(len(images)) + ' : ' + im[0])
 		
 		commande="gdal_translate -of GTiff -a_ullr " + " " + ullon + " " + ullat + " " + lrlon + " " + lrlat + " -a_srs " + srs1 + "  -co COMPRESS=JPEG -co JPEG_QUALITY=100 " + file1 + " " +  file2
-		file = open(name,'w')   # Trying to create a new file or open one
-		file.write('@ECHO OFF\n')
-		file.write('SET Path="c:\program files\qgis pisa\\bin\";%PATH%\n') # !!!!!!!!!!!!!!!!!!
-		file.write('SET GDAL_DATA="c:\georef"\n')
-		file.write(commande + '\n')
-		file.close()
-		os.system(name)
+		os.system(commande)
 		
 		# On supprime les images géoréférencées qui sont vides
 		statinfo = os.stat(file2)
 		taille=statinfo.st_size
 		if taille==8996904: # l'image ne contient que des pixels blancs
 			os.remove(file2)
+			print('image ' + file2 + ' deleted')
 		else:
 			ullon2.append(ullon)
 			ullat2.append(ullat)
@@ -85,13 +72,13 @@ def georef(dossier_in, dossier_out, srs1, srs2):
 	# ============= On supprime les png ====================
 	listing = os.listdir(dossier_out)
 	for infile in listing:
-		nom_iop= dossier_out + '\\' + infile
+		nom_iop= dossier_out + '/' + infile
 		if infile[-3:]=='png':
 			print('image png a supprimer')
 			#os.remove(nom_iop) !!!!!!!!!!!!!!!!!!!
 
 	# ============== on crée un nouveau fichier excel avec les coordonnées des images non vides ========
-	file_output=dossier_out.replace('\\','/') + '/pts_georef_finaux.csv'
+	file_output=dossier_out	 + '/pts_georef_finaux.csv'
 	file = open(file_output,'w')   # Trying to create a new file or open one
 	for i in range(0,len(ullon2)):
 		file.write(str(ullon2[i]) + ' ' + str(ullat2[i]) + '\n')
--- main_program.py
+++ main_program.py
@@ -13,31 +13,34 @@ import imp
 
 
 # ============= Géoréférencement des images =============================================
+path_prog='/home/cvial/data/tuilage/'
+path_drawings='/home/cvial/data/drawings/'
 
 # alternative a l'import : sys.path.append()
-foo=imp.load_source('georef','c:/georef/test8/georef.py') # !!!!!!!!!!!!!!!!!!!!!
-dossier_in=chemin_dwg[:-4]
-dossier_out=chemin_dwg[:-4]
-srs1='"+proj=utm +zone=23 +south +ellps=aust_SA +towgs84=-166.65,100.10,52.88,0,0 +no_defs"'
+foo = imp.load_source('georef', path_prog + 'georef.py')
+nom_dwg = 'a2.dwg'
+dossier_in = path_drawings + nom_dwg[:-4] + '/2_printed_images'
+dossier_out = path_drawings + nom_dwg[:-4] + '/3_georef_images'
+srs1 = '"+proj=utm +zone=23 +south +ellps=aust_SA +towgs84=-166.65,100.10,52.88,0,0 +no_defs"'
 #srs2='"+proj=latlong +ellps=WGS84 +datum=WGS84 +no_defs"'
-srs2='"+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over"'
-foo.georef(dossier_in, dossier_out, srs1, srs2)
-
+srs2 = '"+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over"'
+#foo.georef(dossier_in, dossier_out, srs1, srs2)
 
 # ============= Création des tuiles =========================================
 # ---- on commence par créer le fichier xml qui définit la carte ----
-foo=imp.load_source('xml_carte', 'c:/georef/test8/xml_carte.py')
+foo=imp.load_source('xml_carte', path_prog + 'xml_carte.py')
 dossier_in=dossier_out
-mapfile=dossier_in.replace('\\','/') + '/calepinage.xml'
-foo.xmlage(dossier_in, srs2, mapfile)
+mapfile=dossier_in + '/calepinage.xml'
+points_in=path_drawings + nom_dwg[:-4] + '/3_georef_images/pts_georef_finaux.csv'
+foo.xmlage(dossier_in, srs2, mapfile, points_in)
 
 # ---- puis on crée les tuiles ----
-foo=imp.load_source('__main__', 'generate_tiles_multiprocess.py') #!!!!!!!!!!!!!!!!!!!!!
-tile_dir=dossier_in.replace('\\','/') + '/tiles/'
-minZoom=19
-maxZoom=23
+foo=imp.load_source('__main__', path_prog + 'generate_tiles_multiprocess.py')
+tile_dir=dossier_in.replace('3_georef_images', '4_tiles') + '/'
+minZoom=15
+maxZoom=16
 foo.tuilage(mapfile, tile_dir, minZoom, maxZoom)
-
+print(str(1/0))
 
 # ============= Création d'un fichier avec les temps de création ===============
 foo=imp.load_source('stats', 'stats.py') # !!!!!!!!!!!!!!!!!!
